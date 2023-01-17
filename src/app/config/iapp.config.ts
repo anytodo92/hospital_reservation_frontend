e62761101c2a8b5b87b6ec2eaf0c2a707a26b37b
anytodo92 <anything.todo92@gmail.com>
2023-01-18 01:14:32 +0900

    added app/config

---
+++ src/app/config/iapp.config.ts
@@ -0,0 +1,13 @@
+export interface IAppConfig {
+    routes : any;
+    endpoints : any;
+    topSpecialLimit : number;
+    snackBarDuration : number;
+    tempList : any;
+    tempBlogList : any;
+    tempUserList : any;
+    tempActivityList : any;
+    typeList : any;
+    rateList : any;
+    specTypeList : any;
+}
